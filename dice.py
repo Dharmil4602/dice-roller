c5749edf2868a774c138d7b264d91ba485f35518
Dharmil Shah <dharmilshah476@gmail.com>
2022-04-08 00:28:53 +0530

    Completed the dice simulator with while loop

--- dice.py
+++ dice.py
@@ -2,57 +2,62 @@ import random as r
 # x = r.random(1,6)
 
 ## For printing random number between particular range, we use randint() for integer
-x = r.randint(1,6)
+# x = r.randint(1,6)
 
 ## For printing random number between particular range, we use uniform() for float numbers
 # x = r.uniform(1.0,6.0)
 
-if x == 1:
-    print("-------------")
-    print("|           |")
-    print("|     *     |")
-    print("|           |")
-    print("-------------")
+a = "y"
+while a == "y":
+    x = r.randint(1,6)
+    if x == 1:
+        print("-------------")
+        print("|           |")
+        print("|     *     |")
+        print("|           |")
+        print("-------------")
 
-elif x == 2:
-    print("-------------")
-    print("|           |")
-    print("|     *     |")
-    print("|     *     |")
-    print("|           |")
-    print("-------------")
+    elif x == 2:
+        print("-------------")
+        print("|           |")
+        print("|     *     |")
+        print("|     *     |")
+        print("|           |")
+        print("-------------")
 
-elif x == 3:
-    print("-------------")
-    print("|           |")
-    print("|     *     |")
-    print("|     *     |")
-    print("|     *     |")
-    print("|           |")
-    print("-------------")
+    elif x == 3:
+        print("-------------")
+        print("|           |")
+        print("|     *     |")
+        print("|     *     |")
+        print("|     *     |")
+        print("|           |")
+        print("-------------")
 
-elif x == 4:
-    print("-------------")
-    print("|           |")
-    print("|   *   *   |")
-    print("|   *   *   |")
-    print("|           |")
-    print("-------------")
+    elif x == 4:
+        print("-------------")
+        print("|           |")
+        print("|   *   *   |")
+        print("|   *   *   |")
+        print("|           |")
+        print("-------------")
 
-elif x == 5:
-    print("-------------")
-    print("|           |")
-    print("|   *   *   |")
-    print("|     *     |")
-    print("|   *   *   |")
-    print("|           |")
-    print("-------------")
+    elif x == 5:
+        print("-------------")
+        print("|           |")
+        print("|   *   *   |")
+        print("|     *     |")
+        print("|   *   *   |")
+        print("|           |")
+        print("-------------")
 
-elif x == 6:
-    print("-------------")
-    print("|           |")
-    print("|   *   *   |")
-    print("|   *   *   |")
-    print("|   *   *   |")
-    print("|           |")
-    print("-------------")
+    elif x == 6:
+        print("-------------")
+        print("|           |")
+        print("|   *   *   |")
+        print("|   *   *   |")
+        print("|   *   *   |")
+        print("|           |")
+        print("-------------")
+    
+    x = input("Press Y To Roll Again\n")
